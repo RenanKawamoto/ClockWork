fa00d47e5f6fb56700993cae7b44c554f071c08d
RenanKawamoto <renan_kawamoto@hotmail.com>
2022-05-14 09:14:17 -0300

    Criado o básico da lógica para salvar dados no localStorage

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import {SafeAreaView} from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text } from "react-native";
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import AFazer from './src/pages/AFazer';
 import Fazendo from './src/pages/Fazendo';
@@ -9,7 +12,24 @@ import CriarCard from './src/pages/CriarCard';
 
 const Stack = createNativeStackNavigator();
 
+export const storeData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, value)
+  } catch (e) {
+    // saving error
+  }
+}
+
+export const getData = async (key) => {
+  try {
+    return await AsyncStorage.getItem(key)
+  } catch(e) {
+    // read error
+  }
+}
+
 export default function App() {
+  getData("listaAFazer").then((t) => console.log(t))
   return (
     <SafeAreaView>
       <NavigationContainer>
--- src/pages/CriarCard/index.js
+++ src/pages/CriarCard/index.js
@@ -1,11 +1,35 @@
-import { Text, Button } from "react-native";
+import React from "react";
+import { Text, TextInput, Button } from "react-native";
 import Bar from "../../components/Bar";
+import { storeData, getData } from "../../../App";
 
 export default function CriarCard({navigation})
 {
+    const [nome, onChangeNome] = React.useState("");
     return <>
         <Bar/>
         <Text>CriarCard</Text>
+        <TextInput
+            onChangeText={onChangeNome}
+            placeholder="Nome da atividade"
+            value={nome}
+        />
+
+        <Button title="Criar" onPress={
+            () => {
+                getData("listaAFazer").then((result) => {
+                    if(result == null)
+                    {
+                        var listaAFazerSemTarefas = JSON.stringify({"lista":[]})
+                        storeData("listaAFazer", listaAFazerSemTarefas);
+                        return;
+                    }
+                    var resultJSON = JSON.parse(result)
+                    resultJSON["lista"].push({"Nome": nome, "CheckBox": false, "Checks": []})
+                    storeData("listaAFazer", JSON.stringify(resultJSON))
+                })
+            }
+        }></Button>
         <Button title="Voltar" onPress={()=> navigation.goBack()}/>
     </>
 }
